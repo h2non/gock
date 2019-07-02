27e5f169f725bb33681164ed7ad015be2940c882
Danny Cheung <7041787+danny-cheung@users.noreply.github.com>
2019-07-02 11:59:46 +1000

    Update tests since funcs are not comparable.

--- matcher_test.go
+++ matcher_test.go
@@ -15,14 +15,18 @@ func TestRegisteredMatchers(t *testing.T) {
 
 func TestNewMatcher(t *testing.T) {
 	matcher := NewMatcher()
-	st.Expect(t, matcher.Matchers, Matchers)
-	st.Expect(t, matcher.Get(), Matchers)
+	// Funcs are not comparable, checking slice length as it's better than nothing
+	// See https://golang.org/pkg/reflect/#DeepEqual
+	st.Expect(t, len(matcher.Matchers), len(Matchers))
+	st.Expect(t, len(matcher.Get()), len(Matchers))
 }
 
 func TestNewBasicMatcher(t *testing.T) {
 	matcher := NewBasicMatcher()
-	st.Expect(t, matcher.Matchers, MatchersHeader)
-	st.Expect(t, matcher.Get(), MatchersHeader)
+	// Funcs are not comparable, checking slice length as it's better than nothing
+	// See https://golang.org/pkg/reflect/#DeepEqual
+	st.Expect(t, len(matcher.Matchers), len(MatchersHeader))
+	st.Expect(t, len(matcher.Get()), len(MatchersHeader))
 }
 
 func TestNewEmptyMatcher(t *testing.T) {
