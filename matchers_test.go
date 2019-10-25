5e76ff591cfe3bfd91721d2cd51ec387bfd01369
Paul Bartlett <paul.bartlett@oneiota.co.uk>
2019-10-25 11:29:47 +0100

    When Mocking older third party API's Sometimes they use non standard headers, including UPPERCASE and Mixed-CASE headers. The current matching implementation does not allow for this. This change allows you to mock this type of header, but setting the headers explicitly to the map, rather than through set.

--- matchers_test.go
+++ matchers_test.go
@@ -127,6 +127,8 @@ func TestMatchHeaders(t *testing.T) {
 		{http.Header{"foo": []string{"b(.*)"}}, http.Header{"foo": []string{"barbar"}}, true},
 		{http.Header{"foo": []string{"^bar$"}}, http.Header{"foo": []string{"bar"}}, true},
 		{http.Header{"foo": []string{"^bar$"}}, http.Header{"foo": []string{"barbar"}}, false},
+		{http.Header{"UPPERCASE": []string{"bar"}}, http.Header{"UPPERCASE": []string{"bar"}}, true},
+		{http.Header{"Mixed-CASE": []string{"bar"}}, http.Header{"Mixed-CASE": []string{"bar"}}, true},
 	}
 
 	for _, test := range cases {
