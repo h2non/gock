c62c6ce478bdbed980ad508f7ebc9c11153bf039
dtluna <dtluna@riseup.net>
2020-01-14 14:09:19 +0300

    Reduce the number of goroutines created in TestTryCreatingRacesInNew to 10

--- gock_test.go
+++ gock_test.go
@@ -430,7 +430,7 @@ func TestObserve(t *testing.T) {
 }
 
 func TestTryCreatingRacesInNew(t *testing.T) {
-	for i := 0; i < 100; {
+	for i := 0; i < 10; {
 		go func() {
 			New("http://example.com")
 		}()
